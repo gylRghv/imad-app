65edcc5240bbdaafbc3609cc213898385e2991be
gylRghv <32592021+gylRghv@users.noreply.github.com>
2018-02-21 19:33:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ app.get('/', function (req, res) {
 });
 
 var counter = 0;
-app.get('/counter',funtion (req,res){ });
+app.get('/counter', function (req, res) {
+  counter += 1; 
+  res.send(counter.toString());
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
