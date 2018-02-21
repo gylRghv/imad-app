09e729d01501b90ebb8f36b2cd3b6a01d305f87d
gylRghv <32592021+gylRghv@users.noreply.github.com>
2018-02-21 19:44:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,21 @@
-console.log('Loaded!');
+var button = document.getElementByID('counter');
+
+button.onclick = function(){
+    var request  = new XMLHttpRequest();
+    var counter = 0;
+    request.onreadystatechange = function(){
+        
+      if(request.readyState === XMLHttpRequest.DONE){
+          
+          if(request.status === 200){
+            counter = request.responseText;
+            var span = document.getElemntById('count');  
+            span.innerHtml = counter;  
+          }
+      }  
+        
+    };
+    
+    request.get('http://gylrghv.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
