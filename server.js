dff5f47ef3e20d161ab88b21e32b64a0ea7ad480
gylRghv <32592021+gylRghv@users.noreply.github.com>
2018-03-15 11:57:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get('/', function (req, res) {
 var pool = new Pool(config);
 
 app.get('/test-db', function(req,res){
-    pool.query('SELECT * from articles', function(err,result){
+    pool.query('SELECT * FROM articles', function(err,result){
       if(err){
           res.status(500).send(err.toString());
       }else{
