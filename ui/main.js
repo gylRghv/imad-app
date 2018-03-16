2d79c9e7018217130a70d449fae5012297493a7d
gylRghv <32592021+gylRghv@users.noreply.github.com>
2018-03-16 22:23:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ submit.onclick  = function(){
     var password = document.getElementById('password').value;
     request.open('POST','http://gylrghv.imad.hasura-app.io/login', true);
     request.setRequestHeader('Content-Type','application/json');
-    request.send(JSON.stringify({username: useranme, password: password}));
+    request.send(JSON.stringify({username: username, password: password}));
 
 };
 
