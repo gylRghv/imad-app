fd8305aed440d410df6e550706b18a0c654e1f06
gylRghv <32592021+gylRghv@users.noreply.github.com>
2018-03-16 22:16:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,36 @@
-var button = document.getElementById('counter');
+var submit = document.getElementById('login_btn');
+
+submit.onclick  = function(){
+    
+   
+    //create a request object
+    var request  = new XMLHttpRequest();
+    //capture the response and store it in a variable 
+    request.onreadystatechange = function(){
+        
+      if(request.readyState === XMLHttpRequest.DONE){
+          
+            if(request.status === 200){
+              console.log("user logged in");
+              alert("Log-In successful");
+            } else if (request.status === 403){
+                alert("uername/password is wrong");                
+            } else if (request.status === 500 ){
+                alert("something went wrong on server!");
+            }
+        }
+    };
+    
+    //make a request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    request.open('POST','http://gylrghv.imad.hasura-app.io/login', true);
+    request.send(JSON.stringify({username: useranme, password: password}));
+
+};
+
+
+/*var button = document.getElementById('counter');
 var counter = 0;
 
 button.onclick = function(){
@@ -23,8 +55,8 @@ button.onclick = function(){
     request.open('GET','http://gylrghv.imad.hasura-app.io/counter', true);
     request.send(null);
 };
-
-var submit = document.getElementById('submit_btn');
+*/
+/*var submit = document.getElementById('submit_btn');
 
 submit.onclick  = function(){
     var nameInput = document.getElementById('name');
@@ -54,4 +86,4 @@ submit.onclick  = function(){
     request.open('GET','http://gylrghv.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 
-};
+};*/
